cc82aafcf3b328790bf24a30f929103920b1b075
Andrew Wittrock <andy@wittrock.us>
2014-06-10 19:27:55 -0500

    Remove asset manager and view manager configs

--- config/module.config.php
+++ config/module.config.php
@@ -5,23 +5,11 @@ return array(
 		'models' =>array(
 		),
 	),
-	'asset_manager' => array(
-		'resolver_configs' => array(
-			'paths' => array(
-				__DIR__ . '/../public',
-			),
-		),
-	),
 	'view_helpers' => array(
 		'invokables' => array(
 			'facebookLike' => 'ATPSocial\View\Helper\FacebookLike',
 			'twitterFollow' => 'ATPSocial\View\Helper\TwitterFollow',
 			'twitterFeed' => 'ATPSocial\View\Helper\TwitterFeed',
 		),
-	),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
 	),
 );
