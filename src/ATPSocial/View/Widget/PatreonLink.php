125fb06c53613251ff8951969dd4532109d63638
Andrew Wittrock <andy@wittrock.us>
2014-09-16 00:10:39 -0500

    Remove contructor parameters

--- src/ATPSocial/View/Widget/PatreonLink.php
+++ src/ATPSocial/View/Widget/PatreonLink.php
@@ -5,10 +5,4 @@ namespace ATPSocial\View\Widget;
 class PatreonLink extends \ATPCore\View\Widget
 {
 	protected $_template = "atp-social/widget/patreon-link.phtml";
-	
-	public function __construct($patreonName)
-	{
-		parent::__construct();
-		$this->patreonName = $patreonName;
-	}
 }
--- src/ATPSocial/View/Widget/TWCVoteButton.php
+++ src/ATPSocial/View/Widget/TWCVoteButton.php
@@ -5,10 +5,4 @@ namespace ATPSocial\View\Widget;
 class TWCVoteButton extends \ATPCore\View\Widget
 {
 	protected $_template = "atp-social/widget/twc-vote-button.phtml";
-	
-	public function __construct($siteId)
-	{
-		parent::__construct();
-		$this->siteId = $siteId;
-	}
 }
