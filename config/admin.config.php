6d45dca04427478534b9e0cfb5a1980587f2191c
Andrew Wittrock <andy@wittrock.us>
2014-09-29 03:08:28 -0500

    Refactor social links widgets

--- config/admin.config.php
+++ config/admin.config.php
@@ -11,11 +11,26 @@ return array(
 				'type' => 'Text',
 				'default' => '&lt;Patreon Name Goes Here&gt;',
 			),
+			'patreon-link-image' => array(
+				'displayName' => 'Patreon Link Image',
+				'group' => 'Social',
+				'type' => 'ModelSelect',
+				'default' => '',
+				'options' => array(
+					'className' => 'ATPCms\Model\Image',
+				),
+			),
 			'social-twc-site-id' => array(
 				'displayName' => 'Top Webcomics Site Id',
 				'group' => 'Social',
 				'type' => 'Text',
 				'default' => '&lt;TWC Site Id Goes Here&gt;',
+			),
+			'social-twc-incentive-date' => array(
+				'displayName' => 'Last TWC Incentive Update',
+				'group' => 'Social',
+				'type' => 'Date',
+				'default' => '',
 			),
 		),
 	),
